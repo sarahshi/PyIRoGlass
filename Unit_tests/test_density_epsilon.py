75e2b7817ed8107985f0a061a009015d871f4cfb
Sarah Shi <sarah.c.shi@gmail.com>
2023-03-23 11:57:41 -0400

    Correct the epsilon calculation to be performed on anhydrous basis

--- Unit_tests/test_density_epsilon.py
+++ Unit_tests/test_density_epsilon.py
@@ -7,6 +7,8 @@ class test_density_epsilon_calculation(unittest.TestCase):
     def setUp(self): 
         self.MI_Composition = pd.DataFrame([{'Sample': 'AC4_OL53_101220_256s_30x30_a', 'SiO2': 47.95, 'TiO2': 1.00, 'Al2O3': 18.88, 'Fe2O3': 2.04, 'FeO': 7.45, 'MnO': 0.19, 'MgO': 4.34, 'CaO': 9.84, 'Na2O': 3.47, 'K2O': 0.67, 'P2O5': 0.11, 'H2O': 4.038927}])
         self.MI_Composition.set_index('Sample', inplace = True)
+        self.MI_Composition_dry = pd.DataFrame([{'Sample': 'AC4_OL53_101220_256s_30x30_a', 'SiO2': 47.95, 'TiO2': 1.00, 'Al2O3': 18.88, 'Fe2O3': 2.04, 'FeO': 7.45, 'MnO': 0.19, 'MgO': 4.34, 'CaO': 9.84, 'Na2O': 3.47, 'K2O': 0.67, 'P2O5': 0.11, 'H2O': 0}])
+        self.MI_Composition_dry.set_index('Sample', inplace = True)
         self.T_room = 25 
         self.P_room = 1 
         self.decimalPlace = 3
@@ -18,8 +20,7 @@ class test_density_epsilon_calculation(unittest.TestCase):
         self.assertAlmostEqual(result, expected, self.decimalPlace, msg="Density test and expected values from the Density_Calculation function do not agree")
 
     def test_epsilon_calculation(self):
-        mol, density = pig.Density_Calculation(self.MI_Composition, self.T_room, self.P_room)
-        epsilon = pig.Epsilon_Calc(self.MI_Composition, self.T_room, self.P_room)
+        epsilon = pig.Epsilon_Calc(self.MI_Composition_dry, self.T_room, self.P_room)
         tau = float(epsilon['Tau'])
         expected_tau = 0.682894853
         epsilon_h2ot = float(epsilon['epsilon_H2OT_3550'])
