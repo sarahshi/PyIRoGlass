73441e79674ccf067bfbb75b3720701a45f87244
Sarah Shi <sarah.c.shi@gmail.com>
2023-06-05 01:23:09 -0400

    Revert "Update UnitTests"
    This reverts commit e07b5da7c201cdacd4b7adb80e8a281899e82af3.

--- UnitTests/test_concentration.py
+++ UnitTests/test_concentration.py
@@ -15,8 +15,8 @@ class test_conc_outputs_h2ot(unittest.TestCase):
         self.sigma_density = self.density * 0.025
         self.thickness = 39
         self.sigma_thickness = 3
-        self.epsilon = 64.4628687805379
-        self.sigma_epsilon = 7.40123952105104
+        self.epsilon = 64.46286878
+        self.sigma_epsilon = 7.401239521
         self.N = 500000
         self.MI_Composition = {'SiO2': 47.95, 'TiO2': 1.00, 'Al2O3': 18.88, 'Fe2O3': 2.04, 'FeO': 7.45, 'MnO': 0.19,
                                'MgO': 4.34, 'CaO': 9.84, 'Na2O': 3.47, 'K2O': 0.67, 'P2O5': 0.11}
@@ -42,14 +42,14 @@ class test_conc_outputs_co2(unittest.TestCase):
     def setUp(self): 
 
         self.molar_mass = 44.01
-        self.absorbance = 0.0503435585261241
-        self.sigma_absorbance = 0.00505976348415682
+        self.absorbance = 0.052887397
+        self.sigma_absorbance = 0.005128284
         self.density = 2702.703546
         self.sigma_density = self.density * 0.025
         self.thickness = 39
         self.sigma_thickness = 3
-        self.epsilon = 302.327095954195
-        self.sigma_epsilon = 18.0682300900254
+        self.epsilon = 302.327096
+        self.sigma_epsilon = 18.06823009
         self.N = 500000
         self.MI_Composition = {'SiO2': 47.95, 'TiO2': 1.00, 'Al2O3': 18.88, 'Fe2O3': 2.04, 'FeO': 7.45, 'MnO': 0.19,
                                'MgO': 4.34, 'CaO': 9.84, 'Na2O': 3.47, 'K2O': 0.67, 'P2O5': 0.11}
@@ -60,13 +60,13 @@ class test_conc_outputs_co2(unittest.TestCase):
     def test_beer_lambert(self):
 
         result = pig.Beer_Lambert(self.molar_mass, self.absorbance, self.sigma_absorbance, self.density, self.sigma_density, self.thickness, self.sigma_thickness, self.epsilon, self.sigma_epsilon) * 10000
-        expected = 695.272717493039
+        expected = 730.4045443
         self.assertAlmostEqual(result, expected, self.decimalPlace, msg="CO2_1515 test and expected values from the Beer_Lambert equation do not agree")
 
     def test_beer_lambert_error(self):
         
         result = pig.Beer_Lambert_Error(self.N, self.molar_mass, self.absorbance, self.sigma_absorbance, self.density, self.sigma_density, self.thickness, self.sigma_thickness, self.epsilon, self.sigma_epsilon) * 10000
-        expected = 94.5354743851212
+        expected = 97.35842352      
         self.assertAlmostEqual(result, expected, self.decimalPlace-2, msg="CO2_1515 test and expected errors from the Beer_Lambert_Error equation do not agree")
 
 
@@ -92,7 +92,7 @@ class test_conc_outputs(unittest.TestCase):
 
         density_epsilon, mega_spreadsheet = pig.Concentration_Output(self.PH, self.N, self.thickness, self.MI_Composition, self.T_room, self.P_room)
         expected_H2O = 4.03892743514451
-        expected_CO2 = 696.243084652244
+        expected_CO2 = 730.4045443
         self.assertAlmostEqual(float(mega_spreadsheet['H2OT_MEAN']), expected_H2O, self.decimalPlace-2, msg="H2Ot test values from the Concentration_Output equation do not agree")
         self.assertAlmostEqual(float(mega_spreadsheet['CO2_MEAN']), expected_CO2, self.decimalPlace-2, msg="CO2m test values from the Concentration_Output equation do not agree")
 
