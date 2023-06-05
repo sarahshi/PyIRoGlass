6ceafb0e73b3b0c722577a70a4e20d87d962c151
Sarah Shi <sarah.c.shi@gmail.com>
2023-06-04 23:33:40 -0400

    Update UnitTests

--- UnitTests/test_plotting.py
+++ UnitTests/test_plotting.py
@@ -1,12 +1,13 @@
 import unittest
 from unittest.mock import patch, MagicMock
 import os
+import mc3
 import numpy as np
 import pandas as pd
 import PyIRoGlass as pig
 
 
-class test_plotting(unittest.TestCase):
+class test_plotting_trace(unittest.TestCase):
 
     @patch('numpy.lexsort')
     @patch('numpy.ediff1d')
@@ -14,9 +15,10 @@ class test_plotting(unittest.TestCase):
     @patch('numpy.amax')
     @patch('matplotlib.pyplot.figure')
     @patch('matplotlib.pyplot.subplot')
-    @patch('pig.mu.default_parnames')  # adjust this according to where this function is defined
+    @patch('mc3.utils.mu.default_parnames')
 
     def test_trace(self, mock_default_parnames, mock_subplot, mock_figure, mock_amax, mock_where, mock_ediff1d, mock_lexsort):
+
         mock_default_parnames.return_value = ['a', 'b', 'c']
         mock_amax.return_value = 1
         mock_where.return_value = [np.array([1])]
@@ -37,6 +39,32 @@ class test_plotting(unittest.TestCase):
         fs = 12
 
         result = pig.trace(posterior, 'test', zchain, pnames, thinning, burnin, fignum, savefile, fmt, ms, fs)
+        self.assertIsNotNone(result)  # basic check, adjust this according to what you want to assert
+
+class test_plotting_modelfit(unittest.TestCase):
+    
+    @patch('numpy.size')
+    @patch('matplotlib.pyplot.figure')
+    @patch('matplotlib.pyplot.axes')
+    @patch('mc3.stats.ms.bin_array')  # adjust this according to where this function is defined
+
+    def test_modelfit(self, mock_bin_array, mock_axes, mock_figure, mock_size):
+
+        mock_bin_array.return_value = np.array([1])
+        mock_axes.return_value = MagicMock()
+        mock_figure.return_value = MagicMock()
+        mock_size.return_value = 1
+
+        data = np.array([1,2,3])
+        uncert = np.array([1,2,3])
+        indparams = np.array([1,2,3])
+        model = np.array([1,2,3])
+        nbins = 75
+        fignum = 1400
+        savefile = None
+        fmt = "."
+
+        result = pig.modelfit(data, uncert, indparams, model, 'test', nbins, fignum, savefile, fmt)
         self.assertIsNotNone(result)  # basic check, adjust this according to what you want to assert
 
 
