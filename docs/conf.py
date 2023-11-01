813d0112a01c0fc6b305d34d16effc95e6203d2e
Sarah Shi <sarah.c.shi@gmail.com>
2023-11-01 12:03:07 -0400

    PyIRoGlass v0.3.1 released! Paper submitted!

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@
 import os
 import sys
 import sphinx_rtd_theme
-sys.path.insert(0, os.path.abspath('..'))
-
 
 # -- Project information -----------------------------------------------------
 
@@ -32,7 +30,7 @@ release = 'v.0'
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- General configuration ---------------------------------------------------
