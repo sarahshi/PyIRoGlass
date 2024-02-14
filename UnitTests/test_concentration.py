d97a5ff0c184c89d3522a3708d757091eb581d72
Sarah Shi <sarah.c.shi@gmail.com>
2024-02-14 13:19:36 -0500

    Add SampleDataLoader/VectorLoader classes, modularize plotting, add Iacovino+Till 2019 density, rename Run_All_Spectra as calculate_baselines, lower case functions, fix UnitTests correspondingly
    Former-commit-id: 0f6d976eedcbdac27611aaebe08ff72df6d98c85

--- UnitTests/test_concentration.py
+++ UnitTests/test_concentration.py
@@ -1,12 +1,11 @@
 import unittest
-import numpy as np
 import pandas as pd
 import PyIRoGlass as pig
 
 
 class test_conc_outputs_h2ot(unittest.TestCase):
 
-    def setUp(self): 
+    def setUp(self):
 
         self.molar_mass = 18.01528
         self.absorbance = 1.523342931
@@ -18,28 +17,63 @@ class test_conc_outputs_h2ot(unittest.TestCase):
         self.epsilon = 64.4628687805379
         self.sigma_epsilon = 7.401239521
         self.N = 500000
-        self.MI_Composition = {'SiO2': 47.95, 'TiO2': 1.00, 'Al2O3': 18.88, 'Fe2O3': 2.04, 'FeO': 7.45, 'MnO': 0.19,
-                               'MgO': 4.34, 'CaO': 9.84, 'Na2O': 3.47, 'K2O': 0.67, 'P2O5': 0.11}
-        self.T_room = 25 
-        self.P_room = 1 
+        self.MI_Composition = {
+            'SiO2': 47.95,
+            'TiO2': 1.00,
+            'Al2O3': 18.88,
+            'Fe2O3': 2.04,
+            'FeO': 7.45,
+            'MnO': 0.19,
+            'MgO': 4.34,
+            'CaO': 9.84,
+            'Na2O': 3.47,
+            'K2O': 0.67,
+            'P2O5': 0.11}
+        self.T_room = 25
+        self.P_room = 1
         self.decimalPlace = 5
 
     def test_beer_lambert(self):
 
-        result = pig.Beer_Lambert(self.molar_mass, self.absorbance, self.density, self.thickness, self.epsilon)
+        result = pig.beer_lambert(
+            self.molar_mass,
+            self.absorbance,
+            self.density,
+            self.thickness,
+            self.epsilon)
         expected = 4.03892743514451
-        self.assertAlmostEqual(result, expected, self.decimalPlace, msg="H2Ot test and expected values from the Beer_Lambert function do not agree")
+        self.assertAlmostEqual(
+            result,
+            expected,
+            self.decimalPlace,
+            msg="H2Ot test and expected values from the "
+            "Beer_Lambert function do not agree")
 
     def test_beer_lambert_error(self):
 
-        result = pig.Beer_Lambert_Error(self.N, self.molar_mass, self.absorbance, self.sigma_absorbance, self.density, self.sigma_density, self.thickness, self.sigma_thickness, self.epsilon, self.sigma_epsilon)
+        result = pig.beer_lambert_error(
+            self.N,
+            self.molar_mass,
+            self.absorbance,
+            self.sigma_absorbance,
+            self.density,
+            self.sigma_density,
+            self.thickness,
+            self.sigma_thickness,
+            self.epsilon,
+            self.sigma_epsilon)
         expected = 0.432638234770662
-        self.assertAlmostEqual(result, expected, delta=0.01, msg="H2Ot test and expected errors from the Beer_Lambert_Error function do not agree")
+        self.assertAlmostEqual(
+            result,
+            expected,
+            delta=0.01,
+            msg="H2Ot test and expected errors from the "
+            "Beer_Lambert_Error function do not agree")
 
 
 class test_conc_outputs_co2(unittest.TestCase):
 
-    def setUp(self): 
+    def setUp(self):
 
         self.molar_mass = 44.01
         self.absorbance = 0.053059942737517
@@ -51,77 +85,199 @@ class test_conc_outputs_co2(unittest.TestCase):
         self.epsilon = 296.377038501122
         self.sigma_epsilon = 17.091250588327
         self.N = 500000
-        self.MI_Composition = {'SiO2': 47.95, 'TiO2': 1.00, 'Al2O3': 18.88, 'Fe2O3': 2.04, 'FeO': 7.45, 'MnO': 0.19,
-                               'MgO': 4.34, 'CaO': 9.84, 'Na2O': 3.47, 'K2O': 0.67, 'P2O5': 0.11}
-        self.T_room = 25 
-        self.P_room = 1 
+        self.MI_Composition = {
+            'SiO2': 47.95,
+            'TiO2': 1.00,
+            'Al2O3': 18.88,
+            'Fe2O3': 2.04,
+            'FeO': 7.45,
+            'MnO': 0.19,
+            'MgO': 4.34,
+            'CaO': 9.84,
+            'Na2O': 3.47,
+            'K2O': 0.67,
+            'P2O5': 0.11}
+        self.T_room = 25
+        self.P_room = 1
         self.decimalPlace = 5
 
-    def test_beer_lambert(self): # OL53, 1515 peak
+    def test_beer_lambert(self):  # OL53, 1515 peak
 
-        result = pig.Beer_Lambert(self.molar_mass, self.absorbance, self.density, self.thickness, self.epsilon) * 10000
+        result = pig.beer_lambert(
+            self.molar_mass,
+            self.absorbance,
+            self.density,
+            self.thickness,
+            self.epsilon) * 10000
         expected = 747.498925861367
-        self.assertAlmostEqual(result, expected, self.decimalPlace, msg="CO2_1515 test and expected values from the Beer_Lambert equation do not agree")
+        self.assertAlmostEqual(
+            result,
+            expected,
+            self.decimalPlace,
+            msg="CO2_1515 test and expected values from the "
+            "Beer_Lambert function do not agree")
 
     def test_beer_lambert_error(self):
-        
-        result = pig.Beer_Lambert_Error(self.N, self.molar_mass, self.absorbance, self.sigma_absorbance, self.density, self.sigma_density, self.thickness, self.sigma_thickness, self.epsilon, self.sigma_epsilon) * 10000
+
+        result = pig.beer_lambert_error(
+            self.N,
+            self.molar_mass,
+            self.absorbance,
+            self.sigma_absorbance,
+            self.density,
+            self.sigma_density,
+            self.thickness,
+            self.sigma_thickness,
+            self.epsilon,
+            self.sigma_epsilon) * 10000
         expected = 85.8212335730925
-        self.assertAlmostEqual(result, expected, delta=1, msg="CO2_1515 test and expected errors from the Beer_Lambert_Error equation do not agree")
+        self.assertAlmostEqual(
+            result,
+            expected,
+            delta=1,
+            msg="CO2_1515 test and expected errors from the "
+            "Beer_Lambert_Error function do not agree")
 
 
 class test_conc_outputs(unittest.TestCase):
 
     def setUp(self):
 
-        self.MI_Composition = pd.DataFrame({'SiO2': [47.95], 'TiO2': [1.00], 'Al2O3': [18.88], 'Fe2O3': [2.04], 'FeO': [7.45],
-                              'MnO': [0.19], 'MgO': [4.34], 'CaO': [9.84], 'Na2O': [3.47], 'K2O': [0.67], 'P2O5': [0.11]},
-                              index=['AC4_OL53_101220_256s_30x30_a'])
-        self.PH = pd.DataFrame({'PH_3550_M': [1.52334293070956], 'PH_3550_STD': [0.00330886816920362], 'H2OT_3550_SAT?': ['-'], 'PH_1635_BP': [0.299675069462131],
-                   'PH_1635_STD': [0.00312186211814352], 'PH_1515_BP': [0.053059942737517], 'PH_1515_STD': [0.00376445440661992],
-                   'PH_1430_BP': [0.050243218748645], 'PH_1430_STD': [0.00435858737555569], 'PH_5200_M': [0.00895907201720724], 'PH_5200_STD': [0.000445131906892326], 
-                   'PH_4500_M': [0.0125175731361643], 'PH_4500_STD': [0.000320314738906861], 'S2N_P5200': [6.90415549754328], 'S2N_P4500': [5.52765303774205],
-                   'ERR_5200': ['-'], 'ERR_4500': ['-']}, index=['AC4_OL53_101220_256s_30x30_a'])
-        self.thickness = pd.DataFrame({'Thickness': [39], 'Sigma_Thickness': [3]}, index=['AC4_OL53_101220_256s_30x30_a'])
+        self.MI_Composition = pd.DataFrame(
+            {
+                'SiO2': [47.95],
+                'TiO2': [1.00],
+                'Al2O3': [18.88],
+                'Fe2O3': [2.04],
+                'FeO': [7.45],
+                'MnO': [0.19],
+                'MgO': [4.34],
+                'CaO': [9.84],
+                'Na2O': [3.47],
+                'K2O': [0.67],
+                'P2O5': [0.11]},
+            index=['AC4_OL53_101220_256s_30x30_a'])
+        self.PH = pd.DataFrame(
+            {
+                'PH_3550_M': [1.52334293070956],
+                'PH_3550_STD': [0.00330886816920362],
+                'H2OT_3550_SAT?': ['-'],
+                'PH_1635_BP': [0.299675069462131],
+                'PH_1635_STD': [0.00312186211814352],
+                'PH_1515_BP': [0.053059942737517],
+                'PH_1515_STD': [0.00376445440661992],
+                'PH_1430_BP': [0.050243218748645],
+                'PH_1430_STD': [0.00435858737555569],
+                'PH_5200_M': [0.00895907201720724],
+                'PH_5200_STD': [0.000445131906892326],
+                'PH_4500_M': [0.0125175731361643],
+                'PH_4500_STD': [0.000320314738906861],
+                'S2N_P5200': [6.90415549754328],
+                'S2N_P4500': [5.52765303774205],
+                'ERR_5200': ['-'],
+                'ERR_4500': ['-']},
+            index=['AC4_OL53_101220_256s_30x30_a'])
+        self.thickness = pd.DataFrame({
+            'Thickness': [39],
+            'Sigma_Thickness': [3]},
+            index=['AC4_OL53_101220_256s_30x30_a'])
         self.N = 500000
-        self.T_room = 25 
-        self.P_room = 1 
+        self.T_room = 25
+        self.P_room = 1
         self.decimalPlace = 5
 
-    def test_concentration(self): # OL53
+    def test_concentration(self):  # OL53
 
-        density_epsilon, mega_spreadsheet = pig.Concentration_Output(self.PH, self.thickness, self.MI_Composition, self.N, self.T_room, self.P_room)
+        density_epsilon, mega_spreadsheet = pig.calculate_concentration(
+            self.PH, self.thickness, self.MI_Composition,
+            self.N, self.T_room, self.P_room)
         expected_H2O = 4.03892743514451
         expected_CO2 = 727.658175499597
-        self.assertAlmostEqual(float(mega_spreadsheet['H2OT_MEAN'].iloc[0]), expected_H2O, self.decimalPlace, msg="H2Ot test values from the Concentration_Output equation do not agree")
-        self.assertAlmostEqual(float(mega_spreadsheet['CO2_MEAN'].iloc[0]), expected_CO2, self.decimalPlace, msg="CO2m test values from the Concentration_Output equation do not agree")
+        self.assertAlmostEqual(
+            float(
+                mega_spreadsheet['H2OT_MEAN'].iloc[0]),
+            expected_H2O,
+            self.decimalPlace,
+            msg="H2Ot test values from the calculate_concentration "
+            "function do not agree")
+        self.assertAlmostEqual(
+            float(
+                mega_spreadsheet['CO2_MEAN'].iloc[0]),
+            expected_CO2,
+            self.decimalPlace,
+            msg="CO2m test values from the calculate_concentration "
+            "function do not agree")
 
-class test_conc_outputs_saturated(unittest.TestCase): # OL49
+
+class test_conc_outputs_saturated(unittest.TestCase):  # OL49
 
     def setUp(self):
 
-        self.MI_Composition = pd.DataFrame({'SiO2': [52.34], 'TiO2': [1.04], 'Al2O3': [17.92], 'Fe2O3': [1.93], 'FeO': [7.03],
-                              'MnO': [0.20], 'MgO': [3.63], 'CaO': [7.72], 'Na2O': [4.25], 'K2O': [0.78], 'P2O5': [0.14]},
-                              index=['AC4_OL49_021920_30x30_H2O_a'])
-        self.PH = pd.DataFrame({'PH_3550_M': [2.17224950314409], 'PH_3550_STD': [0.00220916618090868], 'H2OT_3550_SAT?': ['*'], 'PH_1635_BP': [0.658349188362703],
-                   'PH_1635_STD': [0.00307592496318145], 'PH_1515_BP': [0.106864326434928], 'PH_1515_STD': [0.0035555642315165],
-                   'PH_1430_BP': [0.109429135799036], 'PH_1430_STD': [0.00405404583060287], 'PH_5200_M': [0.0249095178877388], 'PH_5200_STD': [0.000801149484503151], 
-                   'PH_4500_M': [0.014569658580663], 'PH_4500_STD': [0.0000638336854435091], 'S2N_P5200': [12.1959313061391], 'S2N_P4500': [8.15732983956986],
-                   'ERR_5200': ['-'], 'ERR_4500': ['-']}, index=['AC4_OL49_021920_30x30_H2O_a'])
-        self.thickness = pd.DataFrame({'Thickness': [91.25], 'Sigma_Thickness': [3]}, index=['AC4_OL49_021920_30x30_H2O_a'])
+        self.MI_Composition = pd.DataFrame(
+            {
+                'SiO2': [52.34],
+                'TiO2': [1.04],
+                'Al2O3': [17.92],
+                'Fe2O3': [1.93],
+                'FeO': [7.03],
+                'MnO': [0.20],
+                'MgO': [3.63],
+                'CaO': [7.72],
+                'Na2O': [4.25],
+                'K2O': [0.78],
+                'P2O5': [0.14]},
+            index=['AC4_OL49_021920_30x30_H2O_a'])
+        self.PH = pd.DataFrame(
+            {
+                'PH_3550_M': [2.17224950314409],
+                'PH_3550_STD': [0.00220916618090868],
+                'H2OT_3550_SAT?': ['*'],
+                'PH_1635_BP': [0.658349188362703],
+                'PH_1635_STD': [0.00307592496318145],
+                'PH_1515_BP': [0.106864326434928],
+                'PH_1515_STD': [0.0035555642315165],
+                'PH_1430_BP': [0.109429135799036],
+                'PH_1430_STD': [0.00405404583060287],
+                'PH_5200_M': [0.0249095178877388],
+                'PH_5200_STD': [0.000801149484503151],
+                'PH_4500_M': [0.014569658580663],
+                'PH_4500_STD': [0.0000638336854435091],
+                'S2N_P5200': [12.1959313061391],
+                'S2N_P4500': [8.15732983956986],
+                'ERR_5200': ['-'],
+                'ERR_4500': ['-']},
+            index=['AC4_OL49_021920_30x30_H2O_a'])
+        self.thickness = pd.DataFrame({
+            'Thickness': [91.25],
+            'Sigma_Thickness': [3]},
+            index=['AC4_OL49_021920_30x30_H2O_a'])
         self.N = 500000
-        self.T_room = 25 
-        self.P_room = 1 
+        self.T_room = 25
+        self.P_room = 1
         self.decimalPlace = 5
 
     def test_concentration(self):
 
-        density_epsilon, mega_spreadsheet = pig.Concentration_Output(self.PH, self.thickness, self.MI_Composition, self.N, self.T_room, self.P_room)
+        density_epsilon, mega_spreadsheet = pig.calculate_concentration(
+            self.PH, self.thickness, self.MI_Composition,
+            self.N, self.T_room, self.P_room)
         expected_H2O = 2.54389275724829
         expected_CO2 = 748.715067109224
-        self.assertAlmostEqual(float(mega_spreadsheet['H2OT_MEAN'].iloc[0]), expected_H2O, self.decimalPlace, msg="H2Ot test values from the saturated Concentration_Output equation do not agree")
-        self.assertAlmostEqual(float(mega_spreadsheet['CO2_MEAN'].iloc[0]), expected_CO2, self.decimalPlace, msg="CO2m test values from the saturated Concentration_Output equation do not agree")
+        self.assertAlmostEqual(
+            float(
+                mega_spreadsheet['H2OT_MEAN'].iloc[0]),
+            expected_H2O,
+            self.decimalPlace,
+            msg="H2Ot test values from the saturated calculate_concentration "
+            "function do not agree")
+        self.assertAlmostEqual(
+            float(
+                mega_spreadsheet['CO2_MEAN'].iloc[0]),
+            expected_CO2,
+            self.decimalPlace,
+            msg="CO2m test values from the saturated calculate_concentration "
+            "function do not agree")
 
 
 if __name__ == '__main__':
-     unittest.main()
+    unittest.main()
--- src/PyIRoGlass/__init__.py
+++ src/PyIRoGlass/__init__.py
@@ -2,22 +2,22 @@
 
 __author__ = 'Sarah Shi'
 
-# import os
-# import warnings
-# import numpy as np
-# import pandas as pd
-# import mc3
+import os
+import warnings
+import numpy as np
+import pandas as pd
+import mc3
 
-# import matplotlib as mpl
-# from matplotlib import pyplot as plt
-# import mc3.plots as mp
-# import mc3.utils as mu
-# import mc3.stats as ms
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import mc3.plots as mp
+import mc3.utils as mu
+import mc3.stats as ms
 
-# from pykrige import OrdinaryKriging
-# import scipy.signal as signal
-# from scipy.linalg import solveh_banded
-# import scipy.interpolate as interpolate
+from pykrige import OrdinaryKriging
+import scipy.signal as signal
+from scipy.linalg import solveh_banded
+import scipy.interpolate as interpolate
 
 from PyIRoGlass.core import *
 from PyIRoGlass.thickness import *
