9c13cf7120b7d5774727ea9c1c5262da3283fe55
Sarah Shi <sarah.c.shi@gmail.com>
2024-02-14 14:23:28 -0500

    Change call to 'H2OT_3550_SAT', test calculate_epsilon unit test solution
    Former-commit-id: e7611e880e28e85bdcc59895c40c5915bf741e87

--- UnitTests/test_density_epsilon.py
+++ UnitTests/test_density_epsilon.py
@@ -50,7 +50,7 @@ class test_density_epsilon_calculation(unittest.TestCase):
     def test_density_calculation(self):
 
         mol, density = pig.calculate_density(
-            self.MI_Composition, T=self.T_room, P=self.P_room)
+            self.MI_Composition, self.T_room, self.P_room)
         result = float(density.values)
         expected = 2702.703558
         self.assertAlmostEqual(
@@ -63,7 +63,7 @@ class test_density_epsilon_calculation(unittest.TestCase):
     def test_epsilon_calculation(self):
 
         epsilon = pig.calculate_epsilon(
-            self.MI_Composition_dry, T=self.T_room, P=self.P_room)
+            self.MI_Composition_dry, self.T_room, self.P_room)
         tau = float(epsilon['Tau'].iloc[0])
         expected_tau = 0.682894853
         epsilon_h2ot = float(epsilon['epsilon_H2OT_3550'].iloc[0])
